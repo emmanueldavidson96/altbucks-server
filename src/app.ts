fa7f6c41dd489bfe4fdb2d5a97fc65b1c5596e1b
Emmanuel <emmanueldavidson524@gmail.com>
2025-01-18 12:32:55 +0100

    Updates

--- src/app.ts
+++ src/app.ts
@@ -12,16 +12,22 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors({
+    origin:"https://altbucks.vercel.app",
+    credentials:true
+}));
+
+// Handle preflight requests
+app.options('*', cors({
+    origin: "https://altbucks.vercel.app",
+    credentials: true,
+  }));
 
 // app.use(cors({
 //     origin:"http://localhost:3000",
 //     credentials:true
 // }));
 
-app.use(cors({
-    origin:"https://altbucks.vercel.app",
-    credentials:true
-}));
 
 
 //Routes
