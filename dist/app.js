ceb265a687d5d720b60286daaf6d2cc8d72c65ae
Emmanuel <emmanueldavidson524@gmail.com>
2025-01-18 12:56:37 +0100

    Updates and Corrections

--- dist/app.js
+++ dist/app.js
@@ -39,7 +39,7 @@ app.use(express_1.default.json());
 app.use((0, morgan_1.default)("dev"));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://altbucks.vercel.app"],
     credentials: true
 }));
 //Routes
