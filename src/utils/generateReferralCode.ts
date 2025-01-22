d083d1436f0adfab36bb0805e11d535203e8492a
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-01-22 08:09:55 +0100

    Update referral code generator

--- src/utils/generateReferralCode.ts
+++ src/utils/generateReferralCode.ts
@@ -1,14 +1,13 @@
 export default function generateReferralCode() {
     const codeLength = 8;
-    let code = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
 
+    let code = '';
     for (let i = 0; i < codeLength; i++) {
-        const randomChar = String.fromCharCode(Math.floor(Math.random() * 62) + 48);
-        if (randomChar > '9') {
-            code += String.fromCharCode(randomChar.charCodeAt(0) + 7);
-        } else {
-            code += randomChar;
-        }
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        code += characters[randomIndex];
     }
+
     return code;
 }
