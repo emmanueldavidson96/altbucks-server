fbc5f85cc30e6b190351ea3a1ea199b068c2179a
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-02-07 07:41:17 +0100

    Revert "Remove smtp configuration"
    This reverts commit 8abef64c0de0642aee685b4b491bad644e401f5f.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -215,10 +215,42 @@ export const RequestPasswordReset:RequestHandler = async (request:Request, respo
         if (!user) {
             throw createHttpError(404, "No registered user with email");
         }
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        user.verificationToken = otp;
+        user.verificationTokenExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
+        console.log(user)
+        await user.save();
+
+        // Send OTP to email
 
         response.status(201).json({
             success: true,
-            message: "Request granted"
+            message: "Verification token sent!"
+        });
+    }catch(error){
+        next(error)
+    }
+}
+
+// Verify password reset token
+export const VerifyResetToken:RequestHandler = async (request:Request, response:Response, next:NextFunction) => {
+    const { email, token } = request.body;
+    try{
+        if(!email || !token){
+            throw createHttpError(409, "Missing parameters!")
+        }
+        const user = await userModel.findOne({
+            email,
+            verificationToken: token,
+            verificationTokenExpiresAt: { $gt: Date.now() }
+        });
+
+        if (!user) {
+            throw createHttpError(404, "Invalid or expired verification token");
+        }
+        response.status(201).json({
+            success: true,
+            message: "Token verified!"
         });
     }catch(error){
         next(error)
@@ -226,25 +258,30 @@ export const RequestPasswordReset:RequestHandler = async (request:Request, respo
 }
 
 export const ResetPassword:RequestHandler = async (request:Request, response:Response, next:NextFunction) => {
-    const { email, confirmPassword, newPassword } = request.body;
+    const { email, token, confirmPassword, newPassword } = request.body;
     try{
-        if(!email || !confirmPassword || !newPassword){
+        if(!email || !token || !confirmPassword || !newPassword){
             throw createHttpError(409, "Missing parameters!")
         }
         if (newPassword !== confirmPassword) {
-            throw createHttpError(400, "Passwords do not match!")
+            response.status(400).json({ message: "Passwords do not match" });
         }
 
         const user = await userModel.findOne({
             email,
+            verificationToken: token,
+            verificationTokenExpiresAt: { $gt: Date.now() }
         });
+        console.log(user)
 
         if (!user) {
             throw createHttpError(400, "Invalid or expired Otp")
         }
         else {
             user.password = await bcrypt.hash(newPassword, 10);
-            await user.save()
+            user.verificationToken = undefined;
+            user.verificationTokenExpiresAt = undefined;
+            await user?.save()
 
             response.status(200).json({
                 success:true,
