af4a8fed23952c716b07cfdf60d465665e21290e
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-02-10 12:17:15 +0100

    Add new deployment to list of allowed urls

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["http://localhost:3000","https://altbucks.vercel.app"],
+    origin:["http://localhost:3000","https://altbucks.vercel.app", "https://altbucks-six.vercel.app"],
     credentials:true
 }));
 
